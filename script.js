0bc77e26b0ccb3ddc1cd6748f753df1eed5fb42c
Danielinoa07 <131998435+Danielinoa07@users.noreply.github.com>
2023-04-27 16:47:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let optionOneButton = document.querySelector(".option-one")
+let optionOneScreen = document.querySelector(".option-one-screen")
+let optionTwoButton = document.querySelector(".option-two")
+let optionTwoScreen = document.querySelector(".option-two-screen")
+let optionOneOne = document.querySelector(".option-one-one")
+
+
+
+
+
+
+
+
+optionOneButton.onclick = function(){
+  optionOneScreen.style.display = "block"
+};
+
+optionTwoButton.onclick=function(){
+  optionTwoScreen.style.display = "block"
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
